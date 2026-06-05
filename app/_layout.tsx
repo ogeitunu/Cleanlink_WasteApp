de905f724e844204f7b5d731e5970027e041f174
Oge <52808493+ogeitunu@users.noreply.github.com>
2026-06-05 18:35:08 +0100

    Update app/layout
    To stop apk crash on testing on android after installing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,26 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/context/AuthContext";
+import { View, ActivityIndicator } from "react-native";
+import { useAuth } from "@/context/AuthContext";
+
+function RootNavigator() {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <RootNavigator />
     </AuthProvider>
   );
 }
