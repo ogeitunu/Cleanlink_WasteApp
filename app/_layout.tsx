b73c1f85b250b88c789aa90252ce855ddad2d3b6
Oge <52808493+ogeitunu@users.noreply.github.com>
2026-06-11 13:24:17 +0100

    updating app/index.tsx
    testing app for any blank screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,5 @@
-import { Stack } from "expo-router";
-import { AuthProvider } from "@/context/AuthContext";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </AuthProvider>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,9 @@
-import { useEffect } from "react";
-import { router, useRootNavigationState } from "expo-router";
-import { useAuth } from "@/context/AuthContext";
-import { View, ActivityIndicator } from "react-native";
+import { View, Text } from 'react-native';
 
 export default function Index() {
-  const navState = useRootNavigationState();
-  const { user, loading } = useAuth();
-
-  useEffect(() => {
-    if (!navState?.key) return;
-    if (loading) return;
-
-    if (!user) {
-      router.replace("/(auth)/login");
-    } else {
-      router.replace("/(app)/dashboard");
-    }
-  }, [navState, user, loading]);
-
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ActivityIndicator />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Text style={{ fontSize: 24, color: 'black' }}>APP WORKS</Text>
     </View>
   );
 }
